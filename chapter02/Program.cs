f639af5dcd251880b081923e07df657c7c6861d5
Elroy Kanye <elroykanye@gmail.com>
2022-03-19 00:44:49 +0100

    done chapter 02

--- chapter02/Program.cs
+++ chapter02/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// exercise 6
+bool isMale = true;
+
+
+// exercise 7
+string var1 = "Hello";
+string var2 = "World";
+object var3 = var1 + " " + var2;
+Console.WriteLine(var3);
+
+
+// exercise 8
+string var4 = (string) var3;
+
+
+// exercise 13
+int num1 = 5;
+int num2 = 10;
+int tmp = num1;
+num1 = num2;
+num2 = num1;
